273199894cfb63351b3bcfe1703e1ebcc2e237b8
Beatriz Sato <beatriztamie@icloud.com>
2024-03-20 18:30:12 -0300

    atualizações

---
+++ script.js
@@ -0,0 +1,87 @@
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+
+  if (tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add("ativo");
+
+    function activeTab(index) {
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+
+  if (accordionList.length) {
+    accordionList[0].classList.add("ativo");
+    accordionList[0].nextElementSibling.classList.add("ativo");
+
+    function activeAccordion() {
+      this.classList.toggle("ativo");
+      this.nextElementSibling.classList.toggle("ativo");
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+
+initAccordion();
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+    section.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+
+initScrollSuave();
+
+const pessoa = {
+  nome: "nome pessoa",
+  idade: 0,
+  andar() {
+    console.log(this.nome + "andou");
+  },
+};
+function Pessoa(nome, idade) {
+  this.nome = nome;
+  this.idade = idade;
+  this.andar = function () {
+    console.log(this.nome + " andou");
+  };
+}
+
+const joao = new Pessoa("joao", 20);
+const julia = new Pessoa("julia", 20);
+const pedro = new Pessoa("pedro", 20);
+
+function Dom(seletor) {
+  const elementList = document.querySelectorAll(seletor);
+  this.elements = elementList;
+}
+const listaItens = new Dom("li");
